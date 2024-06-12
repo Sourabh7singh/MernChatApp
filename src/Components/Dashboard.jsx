6af8d9c247e91ca565e40376a75e1f25b326d60c
Sourabh7singh <souravhsingh2002@gmail.com>
2024-06-12 12:37:34 +0530

    Minor Fixes

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -15,9 +15,9 @@ const Dashboard = () => {
     const ServerUrl = import.meta.env.VITE_SERVER_URL;
     const { fetchUsers, ShowContextMenu, setShowContextMenu,
         coordinates, setCoordinates, FetchConversations, selectedMessage, setSelectedMessage, Conversations, setConversations
-        , messages, setmessages, CurrentChat, setCurrentChat, FetchGroups,searchUsers,
+        , messages, setmessages, CurrentChat, setCurrentChat, FetchGroups, searchUsers,
         setActiveUsers,
-        setSearchUsers,ActiveUsers,
+        setSearchUsers, ActiveUsers,
         socket, setSocket, loading } = useContext(DashboardContext);
     const messageRef = useRef();
     const LoggedInUser = localStorage.getItem("user");
@@ -39,7 +39,7 @@ const Dashboard = () => {
     const [ShowuserProfile, setShowuserProfile] = useState(false);
     const [isLazyloading, setIsLazyloading] = useState(false);
     const [HasMoreMessages, setHasMoreMessages] = useState(true);
-    
+
 
     const ChatContainerRef = useRef();
     const scrollPositionRef = useRef({ scrollTop: 0, scrollHeight: 0 });
@@ -56,8 +56,8 @@ const Dashboard = () => {
         socket?.emit('addUser', userId);
         socket?.on("getUsers", users => {
             console.log("Active Users :>>", users);
-            setActiveUsers(prev=>[]);
-            users.map((user)=>{
+            setActiveUsers(prev => []);
+            users.map((user) => {
                 setActiveUsers(prev => ([...prev, user.userId]));
             })
             // setActiveUsers(users);
@@ -82,12 +82,15 @@ const Dashboard = () => {
             socket?.emit("joinGroup", data);
         }
         socket?.on("getGroupMessage", (data) => {
-            if (CurrentChat._id === data.groupId) {
+            if (!CurrentChat) {
+                return toast(`New Message from group ${data.groupName} by ${data.name}`, { type: "info" });
+            }
+            else if (CurrentChat._id === data.groupId) {
                 if (data?.senderId === userId) { return };
                 setmessages(prev => ([...prev, data]));
             }
             else {
-                toast("New Message");
+                return toast(`New Message from group ${data.groupName} by ${data.name}`, { type: "info" });
             }
         })
         return () => {
@@ -208,7 +211,7 @@ const Dashboard = () => {
     }
     const SendGroupMessage = async (e) => {
         e.preventDefault();
-        socket.emit("sendGroupMessage", { text, senderId: userId, groupId: CurrentChat?._id, date: Date.now() })
+        socket.emit("sendGroupMessage", { text, senderId: userId, groupId: CurrentChat?._id, date: Date.now(), groupName: CurrentChat?.groupName });
         const responce = await fetch(`${ServerUrl}/api/groups/sendmessage`, {
             method: "POST",
             headers: {
@@ -321,7 +324,7 @@ const Dashboard = () => {
                         </div>
                     </div>
                     <div className='search-bar w-full m-3 bg-slate-800 pl-2 pr-2 min-w-[260px] relative'>
-                        <input type="text" className='w-full p-3 rounded-2xl' placeholder='Search...' onChange={(e) => { setSearchUsers(e.target.value) }} value={searchUsers}/>
+                        <input type="text" className='w-full p-3 rounded-2xl' placeholder='Search...' onChange={(e) => { setSearchUsers(e.target.value) }} value={searchUsers} />
                         <div className='cursor-pointer absolute' style={{ right: "15px", top: "10px" }}>
                             {searchUsers && <svg width="25px" height="25px" viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => { setSearchUsers("") }}>
                                 <path d="M6.96967 16.4697C6.67678 16.7626 6.67678 17.2374 6.96967 17.5303C7.26256 17.8232 7.73744 17.8232 8.03033 17.5303L6.96967 16.4697ZM13.0303 12.5303C13.3232 12.2374 13.3232 11.7626 13.0303 11.4697C12.7374 11.1768 12.2626 11.1768 11.9697 11.4697L13.0303 12.5303ZM11.9697 11.4697C11.6768 11.7626 11.6768 12.2374 11.9697 12.5303C12.2626 12.8232 12.7374 12.8232 13.0303 12.5303L11.9697 11.4697ZM18.0303 7.53033C18.3232 7.23744 18.3232 6.76256 18.0303 6.46967C17.7374 6.17678 17.2626 6.17678 16.9697 6.46967L18.0303 7.53033ZM13.0303 11.4697C12.7374 11.1768 12.2626 11.1768 11.9697 11.4697C11.6768 11.7626 11.6768 12.2374 11.9697 12.5303L13.0303 11.4697ZM16.9697 17.5303C17.2626 17.8232 17.7374 17.8232 18.0303 17.5303C18.3232 17.2374 18.3232 16.7626 18.0303 16.4697L16.9697 17.5303ZM11.9697 12.5303C12.2626 12.8232 12.7374 12.8232 13.0303 12.5303C13.3232 12.2374 13.3232 11.7626 13.0303 11.4697L11.9697 12.5303ZM8.03033 6.46967C7.73744 6.17678 7.26256 6.17678 6.96967 6.46967C6.67678 6.76256 6.67678 7.23744 6.96967 7.53033L8.03033 6.46967ZM8.03033 17.5303L13.0303 12.5303L11.9697 11.4697L6.96967 16.4697L8.03033 17.5303ZM13.0303 12.5303L18.0303 7.53033L16.9697 6.46967L11.9697 11.4697L13.0303 12.5303ZM11.9697 12.5303L16.9697 17.5303L18.0303 16.4697L13.0303 11.4697L11.9697 12.5303ZM13.0303 11.4697L8.03033 6.46967L6.96967 7.53033L11.9697 12.5303L13.0303 11.4697Z" fill="#000000" />
